8a881e97c2da4e39a3ef8fc4cad1b594c14ce85c
Ahmed Mamdouh Eldarandaly <ahmedmamdouh430@gmail.com>
2023-01-20 01:47:51 +0200

    main

--- lib/heart_readings/heart_chart.dart
+++ lib/heart_readings/heart_chart.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:charts_flutter/src/text_element.dart' show TextDirection;
 
+import '../API/screen.dart';
+
 class HeartRateChart extends StatelessWidget {
   final List<charts.Series<dynamic, int>> seriesList;
   final bool animate;
+  final GetReading getReads = GetReading();
 
   HeartRateChart(this.seriesList, {required this.animate});
 
   @override
   Widget build(BuildContext context) {
+    // var chartData = getReads.getData();
     return charts.LineChart(
       seriesList,
       animate: animate,
@@ -43,18 +47,9 @@ class HeartRateSeries {
 class HeartRateLineChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final data = [
-      {'time': 1, 'rate': 80},
-      {'time': 2, 'rate': 75},
-      {'time': 3, 'rate': 85},
-      {'time': 4, 'rate': 90},
-      {'time': 5, 'rate': 88},
-      {'time': 6, 'rate': 82},
-      {'time': 7, 'rate': 70},
-      {'time': 8, 'rate': 94},
-      {'time': 9, 'rate': 120},
-      {'time': 10, 'rate': 130},
-    ];
+    // final GetReading getReads = GetReading();
+    var chartData = GetReading.getData();
+    final data = chartData;
     final myHeartRateData =
         data.map((e) => HeartRateSeries(e['time']!, e['rate']!)).toList();
     final seriesList = [
